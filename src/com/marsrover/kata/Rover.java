fd1b90f687f7131b5461ee895e1f97cc2995d303
Abdu Ssekalala <assekalala@gmail.com>
2019-02-09 00:36:50 +0300

    first commit

---
+++ src/com/marsrover/kata/Rover.java
@@ -0,0 +1,28 @@
+package com.marsrover.kata;
+
+public class Rover {
+
+    public Rover() {
+
+    }
+
+    public void setPosition() {
+
+    }
+
+    public void moveForward() {
+
+    }
+
+    public void moveBackward() {
+
+    }
+
+    public void turnLeft() {
+
+    }
+
+    public void turnRight() {
+
+    }
+}
